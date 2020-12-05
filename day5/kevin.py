6b5f6922d2f112e7b8ce5204b8d3118b265f9844
loic-pcpt <loic.souverain@gmail.com>
2020-12-05 16:36:28 +0100

    readability

--- day5/kevin.py
+++ day5/kevin.py
@@ -1,5 +1,9 @@
 with open("input.txt", "r") as f:
-    seats = [int(s[:-1].replace("F", "0").replace("B", "1").replace("L", "0").replace("R", "1"), 2) for s in f.readlines()]
+    seats = [int(s[:-1]
+                 .replace("F", "0")
+                 .replace("B", "1")
+                 .replace("L", "0")
+                 .replace("R", "1"), 2) for s in f.readlines()]
     seats.sort()
     print(seats[-1])  # answer 1
     for i in range(1, len(seats)):
