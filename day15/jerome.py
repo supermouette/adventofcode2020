080f3c9bd2f6b5a6925febe1f8c6a95471151169
loic-pcpt <loic.souverain@gmail.com>
2020-12-15 18:20:55 +0100

    day 15

---
+++ day15/jerome.py
@@ -0,0 +1,20 @@
+from time import time
+t0 = time()
+numbers = [1, 17, 0, 10, 18, 11, 6]
+n = 2020  # part 1
+n = 30000000  # part 2
+
+n_dict = {numbers[i]: [i] for i in range(len(numbers))}
+
+while len(numbers) != n:
+    prev = numbers[-1]
+    if prev in n_dict:
+        id = len(numbers)-1 - n_dict[prev][-1]
+    else:
+        id = 0
+        n_dict[prev] = []
+    n_dict[prev].append(len(numbers) - 1)
+    numbers.append(id)
+
+print(numbers[-1])
+print(time()-t0)
