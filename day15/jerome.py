a9d7f45724edcb835005184ef9786b5f4ecaf3ac
loic-pcpt <loic.souverain@gmail.com>
2020-12-15 19:42:34 +0100

    opti v2: reduce use of len()

--- day15/jerome.py
+++ day15/jerome.py
@@ -4,15 +4,18 @@ numbers = [1, 17, 0, 10, 18, 11, 6]
 n = 2020  # part 1
 n = 30000000  # part 2
 
-n_dict = {numbers[i]: i for i in range(len(numbers))}
+size = len(numbers)
 
-while len(numbers) != n:
+n_dict = {numbers[i]: i for i in range(size)}
+
+while size != n:
     prev = numbers[-1]
     if prev in n_dict:
-        id = len(numbers)-1 - n_dict[prev]
+        id = size - 1 - n_dict[prev]
     else:
         id = 0
-    n_dict[prev] = len(numbers) - 1
+    n_dict[prev] = size - 1
+    size += 1
     numbers.append(id)
 
 print(numbers[-1])
